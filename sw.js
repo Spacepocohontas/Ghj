d2246304bbd4f67a6263e4738c8265c48fc8043e
Galaxia <ohnoitstamara@gmail.com>
2026-09-07 00:29:39 -0400

    Add offline shell service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='nightshade-forge-shell-v1';
+const SHELL=['./','./index.html','./style.css','./media-studio.css','./forge-ui.css'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response.ok){const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));}
+    return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html))));
+});
